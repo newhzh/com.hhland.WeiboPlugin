568438ce4326545139278c06929c38e4aa9a5bc4
newhzh <newhzh@163.com>
2016-01-21 16:59:19 +0800

    ios version-2

--- www/WeiboPlugin.js
+++ www/WeiboPlugin.js
@@ -1,3 +1,16 @@
 /**
  * Created by hzh on 16/1/21.
  */
+var exec = require('cordova/exec');
+
+var WeiboPlugin=function(){};
+
+WeiboPlugin.prototype.wbShare=function(onSuccess,onError,params){
+    exec(onSuccess,onError,'WeiboPlugin','share',params);
+};
+
+WeiboPlugin.prototype.checkWeiboInstalled= function (onSuccess,onError) {
+    exec(onSuccess,onError,'WeiboPlugin','haswb',[]);
+};
+
+module.exports=new WeiboPlugin();
